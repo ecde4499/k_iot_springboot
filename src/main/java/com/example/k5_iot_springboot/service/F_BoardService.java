7a124f36a1486e7e91b6adc9e499cd6a02ce27ca
ecde4499 <ecde4499@gmail.com>
2025-08-22 14:19:43 +0900

    20250822 Board Page

--- src/main/java/com/example/k5_iot_springboot/service/F_BoardService.java
+++ src/main/java/com/example/k5_iot_springboot/service/F_BoardService.java
@@ -4,6 +4,9 @@ import com.example.k5_iot_springboot.dto.F_Board.request.BoardRequestDto;
 import com.example.k5_iot_springboot.dto.F_Board.response.BoardResponseDto;
 import com.example.k5_iot_springboot.dto.ResponseDto;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -13,4 +16,8 @@ public interface F_BoardService {
     ResponseDto<List<BoardResponseDto.SummaryResponse>> getAllBoards();
 
     ResponseDto<BoardResponseDto.DetailResponse> updateBoard(Long boardId, BoardRequestDto.@Valid UpdateRequest request);
+
+    ResponseDto<BoardResponseDto.PageResponse> getBoardsPage(Pageable pageable);
+
+    ResponseDto<BoardResponseDto.SliceResponse> getBoardsByCursor(Long cursorId, @Min(1) @Max(100) int size);
 }
