239653ce138271f02c2567e80446555b3fe557c1
ecde4499 <ecde4499@gmail.com>
2025-09-02 17:44:11 +0900

    20250902 Stock

--- src/main/java/com/example/k5_iot_springboot/config/WebSecurityConfig.java
+++ src/main/java/com/example/k5_iot_springboot/config/WebSecurityConfig.java
@@ -178,6 +178,11 @@ public class WebSecurityConfig {
                             .requestMatchers(HttpMethod.POST, "/api/v1/products/**").hasAnyRole("ADMIN")
                             .requestMatchers(HttpMethod.PUT, "/api/v1/products/**").hasAnyRole("ADMIN")
 
+                            // stocks 접근 제어
+                            .requestMatchers(HttpMethod.GET, "/api/v1/stocks/**").permitAll()
+                            .requestMatchers(HttpMethod.POST,"/api/v1/stocks/**").hasAnyRole("ADMIN","MANAGER")
+                            .requestMatchers(HttpMethod.PUT, "/api/v1/stocks/**").hasAnyRole("ADMIN","MANAGER")
+
                             // ADMIN 전용 권한 관리 API
                             .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
 
