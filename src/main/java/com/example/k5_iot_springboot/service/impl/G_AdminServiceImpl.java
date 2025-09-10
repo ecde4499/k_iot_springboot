923ad75a4c95e6c0f952099f48885e483916eb67
ecde4499 <ecde4499@gmail.com>
2025-09-10 17:13:28 +0900

    20250910 composite key

--- src/main/java/com/example/k5_iot_springboot/service/impl/G_AdminServiceImpl.java
+++ src/main/java/com/example/k5_iot_springboot/service/impl/G_AdminServiceImpl.java
@@ -4,7 +4,9 @@ import com.example.k5_iot_springboot.common.enums.RoleType;
 import com.example.k5_iot_springboot.dto.G_Admin.request.RoleManageRequest;
 import com.example.k5_iot_springboot.dto.G_Admin.response.RoleManageResponse;
 import com.example.k5_iot_springboot.dto.ResponseDto;
+import com.example.k5_iot_springboot.entity.G_Role;
 import com.example.k5_iot_springboot.entity.G_User;
+import com.example.k5_iot_springboot.repository.G_RoleRepository;
 import com.example.k5_iot_springboot.repository.G_UserRepository;
 import com.example.k5_iot_springboot.security.UserPrincipal;
 import com.example.k5_iot_springboot.service.G_AdminService;
@@ -21,6 +23,7 @@ import java.util.Set;
 @Transactional(readOnly = true)
 public class G_AdminServiceImpl implements G_AdminService {
     private final G_UserRepository userRepository;
+    private final G_RoleRepository roleRepository;
 
     @Override
     public ResponseDto<RoleManageResponse.UpdateRolesResponse> replaceRoles(
@@ -31,15 +34,15 @@ public class G_AdminServiceImpl implements G_AdminService {
                 .orElseThrow(() -> new EntityNotFoundException("해당 ID의 user가 없습니다."));
 
         // 2) 전체 교체 - 요청에 @NotEmpty 이므로 최소 1개 이상의 권한을 보장!
-        user.getRoles().clear();
-        req.roles().forEach(user::addRole);
+        user.getUserRoles().clear();
+//        req.roles().forEach(user::addRole);
 
         userRepository.flush();
 
         RoleManageResponse.UpdateRolesResponse data = new RoleManageResponse.UpdateRolesResponse(
                 user.getId(),
                 user.getLoginId(),
-                Set.copyOf(user.getRoles()),
+                Set.copyOf(user.getRoleTypes()),
                 // 방어적 복사 - JPA 엔티티 컬렉션 필드를 DTO 내부에서 조작할 경우 캡슐화 깨짐 위험
                 user.getUpdatedAt()
         );
@@ -52,32 +55,37 @@ public class G_AdminServiceImpl implements G_AdminService {
         G_User user = userRepository.findWithRolesById(req.userId())
                 .orElseThrow(() -> new EntityNotFoundException("해당 ID의 사용자가 없습니다."));
 
-        RoleType added = req.role();
-        user.addRole(added);
+        G_Role role = roleRepository.findById(req.role())
+                .orElseThrow(() -> new EntityNotFoundException("해당 권한을 찾을 수 없습니다."));
+
+        user.grantRole(role);
 
         userRepository.flush();
 
         RoleManageResponse.AddRoleResponse data = new RoleManageResponse.AddRoleResponse(
                 user.getId(),
                 user.getLoginId(),
-                added,
-                Set.copyOf(user.getRoles()),
+                req.role(),
+                Set.copyOf(user.getRoleTypes()),
                 user.getUpdatedAt()
         );
         return ResponseDto.setSuccess("SUCCESS", data);
     }
 
+    // 권한 제거 (권한 회수 - 최소 1개의 권한 보장: 모두 제거되면 USER 유지)
     @Override
     public ResponseDto<RoleManageResponse.RemoveRoleResponse> removeRoles(UserPrincipal principal, RoleManageRequest.@Valid RemoveRoleRequest req) {
         G_User user = userRepository.findWithRolesById(req.userId())
                 .orElseThrow(() -> new EntityNotFoundException("해당 ID의 사용자가 없습니다."));
 
-        RoleType removed = req.role();
-        user.addRole(removed);
+        G_Role role = roleRepository.findById(req.role())
+                .orElseThrow(()-> new EntityNotFoundException("해당 권한을 찾을 수 없습니다."));
+
+        user.revokeRole(role); // orphanRemoval=true -> 고아 객체 자동 제거
 
         // 비워지는 경우 기존 USER 유지(최소 1개 이상의 권한을 가질 것을 보장하는 정책)
-        if (user.getRoles().isEmpty()) {
-            user.addRole(RoleType.USER);
+        if (user.getUserRoles().isEmpty()) {
+            user.grantRole(roleRepository.getReferenceById(RoleType.USER));
         }
 
         userRepository.flush();
@@ -85,8 +93,8 @@ public class G_AdminServiceImpl implements G_AdminService {
         RoleManageResponse.RemoveRoleResponse data = new RoleManageResponse.RemoveRoleResponse(
                 user.getId(),
                 user.getLoginId(),
-                removed,
-                Set.copyOf(user.getRoles()),
+                req.role(),
+                Set.copyOf(user.getRoleTypes()),
                 user.getUpdatedAt()
         );
         return ResponseDto.setSuccess("SUCCESS", data);
